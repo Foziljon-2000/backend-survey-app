864bc1f822cfdad5cea4d5da96b36501fb244657
Fozil <https://github.com/Foziljon-2000>
2025-11-11 16:44:37 +0500

    connections to DB PostgreSQL

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"backend-survey-app/pkg/databases"
+	"fmt"
 
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db := databases.DB()
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		panic("не удалось подключиться: " + err.Error())
+	}
+	fmt.Println("Подключение успешно!")
 }
